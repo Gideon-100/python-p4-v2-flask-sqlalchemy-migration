3ef875659e17a5c9ec7fa1c4f4cab9ac39ad30af
Gideon lenkai <gideonlenkai2021@gmail.com>
2025-09-26 20:52:02 +0300

    first changes

--- server/models.py
+++ server/models.py
@@ -1,14 +1,11 @@
+# server/models.py
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-# contains definitions of tables and associated schema constructs
 metadata = MetaData()
 
-# create the Flask SQLAlchemy extension
 db = SQLAlchemy(metadata=metadata)
-
-# define a model class by inheriting from db.Model.
-
 
 class Employee(db.Model):
     __tablename__ = 'employees'
@@ -19,3 +16,14 @@ class Employee(db.Model):
 
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.salary}>'
+
+class Department(db.Model):
+    __tablename__ = 'departments'  
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    location = db.Column(db.String, nullable=False)  
+
+    def __repr__(self):
+        return f'<Department {self.id}, {self.name}, {self.location}>'
+
